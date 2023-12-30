21412ceb2235246519f8f646eec511f3fc697be8
Tugba Esat <tugbaesat007@gmail.com>
2023-12-30 12:40:16 -0800

    budget card design

--- app/components/BudgetCard.jsx
+++ app/components/BudgetCard.jsx
@@ -1,32 +1,59 @@
 import React from "react";
 import { currencyFormatter } from "../utils";
-import { Wix_Madefor_Text } from "next/font/google";
 
-const BudgetCard = ({ name, amount, max }) => {
+const BudgetCard = ({ name, amount, max, gray }) => {
+  const bgColor = [];
+  if (amount / max < 0.5) {
+    bgColor.push("bg-blue-500", "bg-opacity-40");
+  } else if (amount / max < 0.75) {
+    bgColor.push("bg-yellow-500", "bg-opacity-10");
+  }
+  else {
+    bgColor.push("bg-red-500", "bg-opacity-10");
+
+  }
+  const progressBarColor = [];
+  if (amount / max < 0.5) {
+    progressBarColor.push("bg-blue-500");
+  } else if (amount / max < 0.75) {
+    progressBarColor.push("bg-yellow-500");
+  } else {
+    progressBarColor.push("bg-red-500");
+  }
+
   return (
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-full">
-      <div className="flex justify-between items-center mb-4">
+    <div
+      className={`${bgColor.join(
+        " "
+      )} max-w-sm p-6 border border-gray-500 rounded-lg shadow  w-full`}
+    >
+      <div className="flex justify-between items-center mb-4 ">
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {name}
         </h5>
         <p className="font-normal text-gray-800 dark:text-gray-200">
           <span>{currencyFormatter.format(amount)} </span> /{" "}
-          <span className="font-light text-gray-500 dark:text-gray-500 text-xs">
+          <span className="font-light text-gray-500 dark:text-gray-400 text-xs">
             {currencyFormatter.format(max)}
           </span>
         </p>
       </div>
 
-      <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 mb-4">
-        <div className="bg-blue-600 h-2.5 rounded-full" style={{width: "50%"}}></div>
+      <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 mb-4 overflow-hidden">
+        <div
+          className={`${progressBarColor.join(" ")} h-2.5 rounded-full `}
+          style={{ width: `${(amount / max) * 100}%` }}
+          ></div>
       </div>
+      <div className="flex gap-2 justify-end">
+        <button className="bg-transparent hover:bg-gray-200 hover:bg-opacity-15 text-blue-400 font-semibold hover:text-blue-700 py-2.5 px-5 me-2 mb-2 text-sm border border-blue-400 rounded">
+          Add Expense
+        </button>
 
-      <a
-        href="#"
-        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Read more
-      </a>
+        <button className="bg-transparent hover:bg-gray-200 hover:bg-opacity-15 text-gray-200 font-semibold hover:text-white py-2.5 px-5 me-2 mb-2 text-sm border border-gray-200 rounded">
+          View Expenses
+        </button>
+      </div>
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-slate-400 dark:bg-slate-600 ">
       <Header/>
-      <BudgetCard name="Rent" amount={500} max={1000}/>
+      <BudgetCard name="Rent" amount={1000} max={1000}/>
     </main>
   );
 }

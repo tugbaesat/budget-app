3a12f981db6a2a7259bcc10bd718fe689f007d18
Tugba Esat <tugbaesat007@gmail.com>
2024-01-02 18:55:00 -0800

    add budget modal

--- app/components/AddBudgetModal.jsx
+++ app/components/AddBudgetModal.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
-const AddBudgetModal = () => {
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function AddBudgetModal() {
+  const [open, setOpen] = useState(false);
+  const [inputs, setInputs] = useState({});
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addBudget({
+      name: event.target.name,
+      max: event.target.value,
+    });
+  };
+
   return (
-    <div>AddBudgetModal</div>
-  )
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} className="w-full max-w-xs">
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            className="text-black"
+          >
+            Add New Budget
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <form
+              className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+              onSubmit={handleSubmit}
+            >
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  for="name"
+                >
+                  Name
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="name"
+                  type="text"
+                  required
+                />
+              </div>
+              <div className="mb-6">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  for="number"
+                >
+                  Maximum Budget
+                </label>
+                <input
+                  className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="number"
+                  type="number"
+                  required
+                  min={0}
+                  step={0.01}
+                />
+              </div>
+              <div className="flex items-center justify-end">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Add Budget
+                </button>
+              </div>
+            </form>
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
 }
 
-export default AddBudgetModal
--- app/page.js
+++ app/page.js
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "./components/Header";
 import BudgetCard from "./components/BudgetCard";
 import { BudgetsProvider } from "./contexts/BudgetContexts";
+import AddBudgetModal from "./components/AddBudgetModal";
 export default function Home() {
   return (
     <BudgetsProvider>
       <main className="flex min-h-screen flex-col items-center bg-slate-400 dark:bg-slate-600 ">
+        <AddBudgetModal></AddBudgetModal>
         <Header />
         <BudgetCard name="Rent" amount={1000} max={1000} />
       </main>

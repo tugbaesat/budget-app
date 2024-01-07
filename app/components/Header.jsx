89f8676bd61502d894de1862e169fc14e1ba0c44
Tugba Esat <tugbaesat007@gmail.com>
2024-01-06 18:58:06 -0800

    delete expenses added

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 const Header = ({onClickBudgetButton, onClickExpenseButton}) => {
   return (
-    <header className="flex items-center md:justify-around md:w-full gap-4 p-6">
+    <header className="flex flex-col items-center md:justify-around md:w-full gap-4 p-6">
       <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
         Budgets
       </h1>
-      <div className="flex gap-4">
+      <div className="flex gap-2">
         <button
-          className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          className="bg-transparent hover:bg-gray-200 hover:bg-opacity-15 text-gray-200 font-semibold hover:text-white py-2.5 px-5 me-2 mb-2 text-sm border border-gray-200 rounded"
           onClick={onClickBudgetButton}
         >
           Add Budget
         </button>
         <button
-          className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+         className="bg-transparent hover:bg-gray-200 hover:bg-opacity-15 text-gray-200 font-semibold hover:text-white py-2.5 px-5 me-2 mb-2 text-sm border border-gray-200 rounded"
           onClick={onClickExpenseButton}
         >
           Add Expense
--- app/components/ViewExpensesModal.jsx
+++ app/components/ViewExpensesModal.jsx
@@ -1,13 +1,13 @@
 "use client";
 import React, { useRef, useState } from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import {
   UNCATEGORIZED_BUDGET_ID,
   useBudgets,
 } from "../contexts/BudgetContexts";
+import { currencyFormatter } from "../utils";
 
 const style = {
   position: "absolute",
@@ -21,27 +21,22 @@ const style = {
   p: 4,
 };
 
-export default function ViewExpensesModal({
-  open,
-  budgetId,
-  handleOpenClose,
-}) {
+export default function ViewExpensesModal({ open, budgetId, handleOpenClose }) {
   const {
     budgets,
-    expenses,
     getBudgetExpenses,
     addBudget,
     addExpense,
     deleteBudget,
     deleteExpense,
   } = useBudgets();
+  const expenses = getBudgetExpenses(budgetId);
 
   const budget =
     UNCATEGORIZED_BUDGET_ID === budgetId
       ? { name: "Uncategorized", id: UNCATEGORIZED_BUDGET_ID }
       : budgets.find((b) => b.id === budgetId);
 
-      console.log(budget)
   return (
     <div>
       <Modal
@@ -50,19 +45,22 @@ export default function ViewExpensesModal({
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style} className="w-full max-w-xs">
+        <Box
+          sx={style}
+          className="w-full max-w-xs flex flex-col gap-4 justify-between "
+        >
           <Typography
             id="modal-modal-title"
             variant="h6"
             component="h2"
-            className="text-black flex gap-4 items-center"
+            className="text-black flex gap-4 items-center justify-between"
           >
-            Expenses - {budget?.name}
+            <span className="text-sm">{budget?.name} Expenses</span>
             {budgetId !== UNCATEGORIZED_BUDGET_ID && (
               <button
-                className="bg-red-500 hover:bg-red-700 text-white p-2  rounded focus:outline-none focus:shadow-outline"
+                className="bg-red-500 hover:bg-red-700 text-white p-2  rounded focus:outline-none focus:shadow-outline text-sm"
                 onClick={() => {
-                  deleteBudget(budgetId);
+                  deleteBudget({ id: budgetId });
                   handleOpenClose();
                 }}
               >
@@ -70,6 +68,25 @@ export default function ViewExpensesModal({
               </button>
             )}
           </Typography>
+          {expenses.map((expense) => (
+            <div
+              className="text-black flex justify-between items-center border-b pb-2 text-sm"
+              key={expense.id}
+            >
+              <p>{expense.description}</p>
+              <div className="flex gap-2 items-center">
+                <p>{currencyFormatter.format(expense.amount)}</p>
+                <button
+                  className="bg-transparent border border-red-500 hover:bg-red-500 hover:text-white text-red-500 rounded p-2 focus:outline-none focus:shadow-outline"
+                  onClick={() => {
+                    deleteExpense(expense);
+                  }}
+                >
+                  &times;
+                </button>
+              </div>
+            </div>
+          ))}
         </Box>
       </Modal>
     </div>
